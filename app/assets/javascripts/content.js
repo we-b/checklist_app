b58e90af435337567cc17bf9b88cb4efd553fcd3
naoki mochizuki <naoki.mochizuki1006@gmail.com>
2015-11-09 17:53:57 +0900

    チェックに漏れがないかを判定するJSファイル作成

---
+++ app/assets/javascripts/content.js
@@ -0,0 +1,13 @@
+//チェック完了or未チェックありを判断
+$(function(){
+  $('#check_button').on('click', function(){
+    var checked = $('input[id="text"]:checked').length;
+    var contents = $('input[id="text"]').length;
+    if(checked === contents){
+      alert('チェック完了です！ お疲れ様でした。');
+    }else{
+      if(!confirm('チェックに漏れがあります。 完了してしまってもよろしいですか？')){
+      return false;
+    };
+  });
+});
