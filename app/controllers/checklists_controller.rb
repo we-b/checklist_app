dc6d4485d6a4e095a1e157196d9d2fea0ec97aed
naoki mochizuki <naoki.mochizuki1006@gmail.com>
2015-11-18 21:01:45 +0900

    コントローラー。　値の呼び出し方法の変更。privateの定義。その他諸々。

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -10,29 +10,29 @@ class ChecklistsController < ApplicationController
   def new
     @checklist = Checklist.new
     @contents = @checklist.contents.build
-    @frequency = Checklist.get_frequency_list
-    @wday = Checklist.get_wday_list
+    @frequency = Settings.frequency
+    @wday = Settings.wday
   end
 
   def show
-    @checklist = Checklist.find(params[:id])
+    @checklist = Checklist.set_checklist(params)
     @contents = @checklist.contents
     flash.now[:"#{@checklist.decide_flash_key}"] = "#{@checklist.decide_flash_message}"
   end
 
   def edit
     @checklist = Checklist.find(params[:id])
-    @frequency = Checklist.get_frequency_list
-    @wday = Checklist.get_wday_list
+    @frequency = Settings.frequency
+    @wday = Settings.wday
   end
 
   def update
     @checklist = Checklist.find(params[:id])
-    if params.require(:checkflag).to_i == 1
+    if params.require(:checkflag).to_s == 'edit_checklist'
       @checklist.check_contents
       redirect_to :root, success: 'チェックが完了しました'
     else
-      @checklist.update(create_params)
+      @checklist.update(checklist_params)
       @checklist.edit_contents(params, contents_params)
       redirect_to :root, success: 'チェックリストの編集が完了しました'
     end
@@ -44,14 +44,16 @@ class ChecklistsController < ApplicationController
   end
 
   def create
-    @checklist = Checklist.create_checklist(create_params)
+    @checklist = Checklist.create_checklist(checklist_params)
     @checklist.check_today
     create_list_contents(@checklist, contents_params)
     @checklist.save ? (redirect_to root_path, success: 'チェックリストの作成が完了しました') : (redirect_to back,  warning: "チェックリストの作成に失敗しました")
   end
 
+  private
 
-  def create_params
+
+  def checklist_params
     params.require(:checklist).permit(:name, :frequency, :date, :wday, :maker, :image, :done)
   end
 
@@ -66,5 +68,6 @@ class ChecklistsController < ApplicationController
   def create_list_contents(checklist, elements)
     elements.each { |ele| checklist.contents.create(text: ele[:text]) }
   end
+
 
 end
