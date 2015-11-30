ce67b9eea46bffaf6c5cac55204bc27b3673998f
naoki mochizuki <naoki.mochizuki1006@gmail.com>
2015-11-30 15:29:59 +0900

    カラムのデフォルト値を変更

--- db/migrate/20151022051234_create_checklists.rb
+++ db/migrate/20151022051234_create_checklists.rb
@@ -7,7 +7,7 @@ class CreateChecklists < ActiveRecord::Migration
       t.integer :wday
       t.string :maker
       t.string :image
-      t.boolean :done, default: true
+      t.boolean :done, default: false
       t.integer :todayflag, dafault: 0
       t.timestamps
     end
